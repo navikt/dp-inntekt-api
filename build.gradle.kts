9fdd680df185d7d125e3f70ab510ed8edeb11ecd
Geir André Lund <geir.andre.lund@nav.no>
2019-01-11 09:58:39 +0100

    Added Jenkinsfile and nais file config. - Build docker image from project
    This resolves navikt/dagpenger#35

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ repositories {
 }
 
 application {
-    applicationName = "dagpenger-inntekt-api"
-    mainClassName = "no.nav.dagpenger.inntekt.Api"
+    applicationName = "dp-inntekt-api"
+    mainClassName = "no.nav.dagpenger.inntekt.AppKt"
 }
 
 java {
@@ -34,6 +34,8 @@ java {
 
 val ktorVersion = "1.1.1"
 val kotlinLoggingVersion = "1.4.9"
+val jupiterVersion = "5.3.2"
+val log4j2Version = "2.11.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -42,11 +44,16 @@ dependencies {
     implementation("io.ktor:ktor-gson:$ktorVersion")
     implementation("io.ktor:ktor-auth:$ktorVersion")
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+    implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
+    implementation("org.apache.logging.log4j:log4j-core:$log4j2Version")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
+    implementation("com.vlkan.log4j2:log4j2-logstash-layout-fatjar:0.15")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    testImplementation("junit:junit:4.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
 }
 
 spotless {
@@ -60,6 +67,7 @@ spotless {
 }
 
 tasks.withType<Test> {
+    useJUnitPlatform()
     testLogging {
         showExceptions = true
         showStackTraces = true
