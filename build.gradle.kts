55517a632b01d836ad40e1e7f78fab92549590a6
Geir André Lund <geir.andre.lund@nav.no>
2019-01-16 14:18:22 +0100

    Use vintage junit engine - will run "old" junit stuff (pre junit 5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$jupiterVersion")
 }
 
 spotless {
