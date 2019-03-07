c1130254134c2fecd52003d6ebdd4af15e6931c4
Atle Nordland <atle.nordland@nav.no>
2019-03-07 13:05:19 +0100

    Add hashmap cache for inntekt

--- src/main/kotlin/no/nav/dagpenger/inntekt/v1/InntektRoute.kt
+++ src/main/kotlin/no/nav/dagpenger/inntekt/v1/InntektRoute.kt
@@ -7,36 +7,60 @@ import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.post
 import io.ktor.routing.route
+import mu.KotlinLogging
 import no.nav.dagpenger.inntekt.InntektskomponentClient
+import no.nav.dagpenger.inntekt.klassifisering.Inntekt
 import no.nav.dagpenger.inntekt.klassifisering.klassifiserInntekter
 import java.time.LocalDate
 import java.time.YearMonth
 
-val MAX_INNTEKT_PERIODE = 38L
+private val MAX_INNTEKT_PERIODE = 38L
+private val LOGGER = KotlinLogging.logger {}
+
+private val inntektCache = HashMap<InntektKey, Inntekt>()
 
 fun Routing.inntekt(inntektskomponentClient: InntektskomponentClient) {
     route("v1/inntekt") {
         post {
             val requestBody = call.receive<InntektRequest>()
 
-            val beregningsDatoMonth = YearMonth.of(requestBody.beregningsDato.year, requestBody.beregningsDato.month)
-            val earliestMonth = beregningsDatoMonth.minusMonths(MAX_INNTEKT_PERIODE)
+            val inntektKey = InntektKey(requestBody.beregningsDato, requestBody.vedtakId)
 
-            val uklassifisertInntekt = inntektskomponentClient.getInntekt(
-                requestBody.aktørId,
-                earliestMonth,
-                beregningsDatoMonth
-            )
-
-            val klassifiserteInntekter = klassifiserInntekter(uklassifisertInntekt)
+            val klassifiserteInntekter = inntektCache[inntektKey]?.let { it } ?: run {
+                val newInntekt = getInntektFromInntektskomponenten(inntektskomponentClient, requestBody)
+                inntektCache[inntektKey] = newInntekt
+                newInntekt
+            }
 
             call.respond(HttpStatusCode.OK, klassifiserteInntekter)
         }
     }
 }
 
+fun getInntektFromInntektskomponenten(
+    inntektskomponentClient: InntektskomponentClient,
+    requestBody: InntektRequest
+): Inntekt {
+    LOGGER.info("Fetching new inntekt for $requestBody")
+    val beregningsDatoMonth = YearMonth.of(requestBody.beregningsDato.year, requestBody.beregningsDato.month)
+    val earliestMonth = beregningsDatoMonth.minusMonths(MAX_INNTEKT_PERIODE)
+
+    val uklassifisertInntekt = inntektskomponentClient.getInntekt(
+        requestBody.aktørId,
+        earliestMonth,
+        beregningsDatoMonth
+    )
+
+    return klassifiserInntekter(uklassifisertInntekt)
+}
+
 data class InntektRequest(
     val aktørId: String,
     val vedtakId: String,
     val beregningsDato: LocalDate
+)
+
+data class InntektKey(
+    val beregningsDato: LocalDate,
+    val vedtaksId: String
 )
