dcb7abc9caf048897e02ebb519fc568c3d959564
Geir André Lund <geir.andre.lund@nav.no>
2019-07-02 14:46:24 +0200

    Fix test

--- src/test/kotlin/no/nav/dagpenger/inntekt/klassifisering/PredicatesArbeidsinntektTest.kt
+++ src/test/kotlin/no/nav/dagpenger/inntekt/klassifisering/PredicatesArbeidsinntektTest.kt
@@ -14,6 +14,7 @@ internal class PredicatesArbeidsinntektTest {
         val predicates = predicatesInntektklasseArbeid()
 
         val arbeidPredicates = listOf(
+            ::isTips,
             ::isLønnAksjerGrunnfondsbevisTilUnderkurs,
             ::isLønnAnnet,
             ::isLønnArbeidsoppholdKost,
