60282b6595e5a7ab187e8a9a4b4301a918aa3e63
Geir André Lund <geir.andre.lund@nav.no>
2019-07-09 09:09:27 +0200

    Update constants

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -6,33 +6,14 @@
  *
  */
 
-// Core dependencies
-object Kotlin  {
-    const val version = "1.3.41"
-    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
-
-    object Logging {
-        const val version = "1.6.22"
-        const val kotlinLogging = "io.github.microutils:kotlin-logging:$version"
-    }
+object Bekk {
+    const val nocommons = "no.bekk.bekkopen:nocommons:0.8.2"
 }
 
-object Spotless {
-    const val version = "3.13.0"
-    const val spotless = "com.diffplug.gradle.spotless"
-}
-
-object Shadow {
-    const val version = "4.0.3"
-    const val shadow = "com.github.johnrengelman.shadow"
-}
-
-object Moshi {
-    const val version = "1.8.0"
-    const val moshi = "com.squareup.moshi:moshi:$version"
-    const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:$version"
-    const val moshiAdapters = "com.squareup.moshi:moshi-adapters:$version"
-    const val moshiKtor = "com.ryanharter.ktor:ktor-moshi:1.0.1"
+object Cucumber {
+    const val version = "4.0.0"
+    const val java8 = "io.cucumber:cucumber-java8:$version"
+    const val junit = "io.cucumber:cucumber-junit:$version"
 }
 
 object Dagpenger {
@@ -44,13 +25,8 @@ object Dagpenger {
         const val ktorUtils = "com.github.navikt.dp-biblioteker:ktor-utils:$version"
     }
 
-    object Streams {
-        const val streams = "com.github.navikt.dagpenger-streams:2019.07.02-10.26.6b16de2e090f"
-    }
-
-    object Events {
-        const val events = "com.github.navikt.dagpenger-events:2019.06.26-21.18.5669e6a90cf3"
-    }
+    const val Streams = "com.github.navikt:dagpenger-streams:2019.07.02-10.26.6b16de2e090f"
+    const val Events = "com.github.navikt:dagpenger-events:2019.06.26-21.18.5669e6a90cf3"
 }
 
 object Database {
@@ -59,6 +35,12 @@ object Database {
     const val Flyway = "org.flywaydb:flyway-core:6.0.0-beta2"
     const val HikariCP = "com.zaxxer:HikariCP:3.3.1"
     const val VaultJdbc = "no.nav:vault-jdbc:1.3.1"
+}
+
+object Fuel {
+    const val version = "2.1.0"
+    const val fuel = "com.github.kittinunf.fuel:fuel:$version"
+    const val fuelMoshi = "com.github.kittinunf.fuel:fuel-moshi:$version"
 }
 
 object Junit5 {
@@ -70,40 +52,43 @@ object Junit5 {
     const val kotlinRunner = "io.kotlintest:kotlintest-runner-junit5:3.3.0"
 }
 
-object TestContainers {
-    const val version = "1.10.6"
-    const val postgresql = "org.testcontainers:postgresql:$version"
+object JsonAssert {
+    const val version = "1.5.0"
+    const val jsonassert = "org.skyscreamer:jsonassert:$version"
 }
 
-object Wiremock {
-    const val version = "2.21.0"
-    const val standalone = "com.github.tomakehurst:wiremock-standalone:$version"
+object Kafka {
+    const val version = "2.0.1"
+    const val clients = "org.apache.kafka:kafka-clients:$version"
+    const val streams = "org.apache.kafka:kafka-streams:$version"
+    const val streamTestUtils = "org.apache.kafka:kafka-streams-test-utils:$version"
+
+    object Confluent {
+        const val version = "5.0.2"
+        const val avroStreamSerdes = "io.confluent:kafka-streams-avro-serde:$version"
+    }
+}
+
+object KafkaEmbedded {
+    const val env = "no.nav:kafka-embedded-env:2.0.2"
+}
+
+object Klint {
+    const val version = "0.31.0"
 }
 
 object Konfig {
     const val konfig = "com.natpryce:konfig:1.6.10.0"
 }
 
-object Mockk {
-    const val version = "1.9.3"
-    const val mockk = "io.mockk:mockk:$version"
-}
+object Kotlin {
+    const val version = "1.3.41"
+    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
 
-object Fuel {
-    const val version = "2.1.0"
-    const val fuel = "com.github.kittinunf.fuel:fuel:$version"
-    const val fuelMoshi = "com.github.kittinunf.fuel:fuel-moshi:$version"
-}
-
-object Prometheus {
-    const val version = "0.6.0"
-    const val common = "io.prometheus:simpleclient_common:$version"
-    const val hotspot = "io.prometheus:simpleclient_hotspot:$version"
-    const val log4j2 =  "io.prometheus:simpleclient_log4j2:$version"
-}
-
-object Bekk {
-    const val nocommons = "no.bekk.bekkopen:nocommons:0.8.2"
+    object Logging {
+        const val version = "1.6.22"
+        const val kotlinLogging = "io.github.microutils:kotlin-logging:$version"
+    }
 }
 
 object Kotlinx {
@@ -116,18 +101,9 @@ object Ktor {
     const val serverNetty = "io.ktor:ktor-server-netty:$version"
     const val auth = "io.ktor:ktor-auth:$version"
     const val authJwt = "io.ktor:ktor-auth-jwt:$version"
+    const val locations = "io.ktor:ktor-locations:$version"
     const val micrometerMetrics = "io.ktor:ktor-metrics-micrometer:$version"
     const val ktorTest = "io.ktor:ktor-server-test-host:$version"
-}
-
-object Micrometer {
-    const val version = "1.1.5"
-    const val prometheusRegistry = "io.micrometer:micrometer-registry-prometheus:$version"
-}
-
-object Ulid {
-    const val version = "8.2.0"
-    const val ulid = "de.huxhorn.sulky:de.huxhorn.sulky.ulid:$version"
 }
 
 object Log4j2 {
@@ -142,11 +118,75 @@ object Log4j2 {
     }
 }
 
-object JsonAssert {
-    const val version = "1.5.0"
-    const val jsonassert = "org.skyscreamer:jsonassert:$version"
+object Micrometer {
+    const val version = "1.1.5"
+    const val prometheusRegistry = "io.micrometer:micrometer-registry-prometheus:$version"
 }
 
-object Klint {
-    const val version = "0.31.0"
+object Moshi {
+    const val version = "1.8.0"
+    const val moshi = "com.squareup.moshi:moshi:$version"
+    const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:$version"
+    const val moshiAdapters = "com.squareup.moshi:moshi-adapters:$version"
+    const val moshiKtor = "com.ryanharter.ktor:ktor-moshi:1.0.1"
 }
+
+object Mockk {
+    const val version = "1.9.3"
+    const val mockk = "io.mockk:mockk:$version"
+}
+
+object Nare {
+    const val version = "768ae37"
+    const val nare = "no.nav:nare:$version"
+}
+
+object Prometheus {
+    const val version = "0.6.0"
+    const val common = "io.prometheus:simpleclient_common:$version"
+    const val hotspot = "io.prometheus:simpleclient_hotspot:$version"
+    const val log4j2 = "io.prometheus:simpleclient_log4j2:$version"
+
+    object Nare {
+        const val version = "0b41ab4"
+        const val prometheus = "no.nav:nare-prometheus:$version"
+    }
+}
+
+object Slf4j {
+    const val version = "1.7.25"
+    const val api = "org.slf4j:slf4j-api:$version"
+}
+
+object Spotless {
+    const val version = "3.13.0"
+    const val spotless = "com.diffplug.gradle.spotless"
+}
+
+object Shadow {
+    const val version = "4.0.3"
+    const val shadow = "com.github.johnrengelman.shadow"
+}
+
+object TestContainers {
+    const val version = "1.10.6"
+    const val postgresql = "org.testcontainers:postgresql:$version"
+    const val kafka = "org.testcontainers:kafka:$version"
+}
+
+object Ulid {
+    const val version = "8.2.0"
+    const val ulid = "de.huxhorn.sulky:de.huxhorn.sulky.ulid:$version"
+}
+
+object Vault {
+    const val javaDriver = "com.bettercloud:vault-java-driver:3.1.0"
+}
+
+object Wiremock {
+    const val version = "2.21.0"
+    const val standalone = "com.github.tomakehurst:wiremock-standalone:$version"
+}
+
+
+
