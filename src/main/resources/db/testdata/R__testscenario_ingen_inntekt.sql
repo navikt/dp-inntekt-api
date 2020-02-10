1e508a61ec10cf9a72625da370d75c4231e881f4
Geir André Lund <geir.andre.lund@nav.no>
2020-02-10 18:54:33 +0100

    Fix INGEN_INNTEKT scenario

--- src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
+++ src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
@@ -1,57 +1,19 @@
-INSERT INTO inntekt_v1(id, brukt, inntekt)
-VALUES ('01D8FSKGXWHE1KBXBSR9BBFAMA', TRUE, '
-{
-  "arbeidsInntektMaaned": [],
-  "ident": {
-    "identifikator": "-1",
-    "aktoerType": "NATURLIG_IDENT"
-  }
-}')
+INSERT INTO inntekt_v1(id, inntekt)
+VALUES ('01D8FSKGXWHE1KBXBSR9BBFAMA', '{}')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO inntekt_v1_arena_mapping (inntektid, aktørid, vedtakid, beregningsdato)
 VALUES ('01D8FSKGXWHE1KBXBSR9BBFAMA', 'INGEN_INNTEKT', 12345, '2019-07-01')
 ON CONFLICT DO NOTHING;
 
-UPDATE inntekt_v1 set inntekt = '
+UPDATE inntekt_v1
+SET inntekt = '
 {
-  "arbeidsInntektMaaned": [
-    {
-      "aarMaaned": "2019-01",
-      "arbeidsInntektInformasjon": {
-        "inntektListe": [
-          {
-            "inntektType": "NAERINGSINNTEKT",
-            "beloep": 250000,
-            "fordel": "kontantytelse",
-            "inntektskilde": "A-ordningen",
-            "inntektsperiodetype": "Maaned",
-            "inntektsstatus": "LoependeInnrapportert",
-            "leveringstidspunkt": "2019-02",
-            "utbetaltIMaaned": "2018-03",
-            "opplysningspliktig": {
-              "identifikator": "1111111",
-              "aktoerType": "ORGANISASJON"
-            },
-            "virksomhet": {
-              "identifikator": "1111111",
-              "aktoerType": "ORGANISASJON"
-            },
-            "inntektsmottaker": {
-              "identifikator": "99999999999",
-              "aktoerType": "NATURLIG_IDENT"
-            },
-            "inngaarIGrunnlagForTrekk": true,
-            "utloeserArbeidsgiveravgift": true,
-            "informasjonsstatus": "InngaarAlltid",
-            "beskrivelse": "lottKunTrygdeavgift"
-          }
-        ]
-      }
-    }],
+  "arbeidsInntektMaaned": [],
   "ident": {
     "identifikator": "-1",
     "aktoerType": "NATURLIG_IDENT"
   }
-}' WHERE id = '01D8FSKGXWHE1KBXBSR9BBFAMA';
+}' , brukt = TRUE
+WHERE id = '01D8FSKGXWHE1KBXBSR9BBFAMA';
 
