19c969272ae8e37fc0b9bd9478a4e8543ca8f3df
Geir André Lund <geir.andre.lund@nav.no>
2020-02-10 15:16:32 +0100

    Second try - cucumber test should fail

--- src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
+++ src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
@@ -11,4 +11,47 @@ ON CONFLICT DO NOTHING;
 
 INSERT INTO inntekt_v1_arena_mapping (inntektid, aktørid, vedtakid, beregningsdato)
 VALUES ('01D8FSKGXWHE1KBXBSR9BBFAMA', 'INGEN_INNTEKT', 12345, '2019-07-01')
-ON CONFLICT DO NOTHING;;
+ON CONFLICT DO NOTHING;
+
+UPDATE inntekt_v1 set inntekt = '
+{
+  "arbeidsInntektMaaned": [
+    {
+      "aarMaaned": "2019-01",
+      "arbeidsInntektInformasjon": {
+        "inntektListe": [
+          {
+            "inntektType": "NAERINGSINNTEKT",
+            "beloep": 250000,
+            "fordel": "kontantytelse",
+            "inntektskilde": "A-ordningen",
+            "inntektsperiodetype": "Maaned",
+            "inntektsstatus": "LoependeInnrapportert",
+            "leveringstidspunkt": "2019-02",
+            "utbetaltIMaaned": "2018-03",
+            "opplysningspliktig": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "virksomhet": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "inntektsmottaker": {
+              "identifikator": "99999999999",
+              "aktoerType": "NATURLIG_IDENT"
+            },
+            "inngaarIGrunnlagForTrekk": true,
+            "utloeserArbeidsgiveravgift": true,
+            "informasjonsstatus": "InngaarAlltid",
+            "beskrivelse": "lottKunTrygdeavgift"
+          }
+        ]
+      }
+    }],
+  "ident": {
+    "identifikator": "-1",
+    "aktoerType": "NATURLIG_IDENT"
+  }
+}' WHERE id = '01D8FSKGXWHE1KBXBSR9BBFAMA';
+
