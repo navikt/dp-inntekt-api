817e5a741b7ae2cd3e72fff00b7a452d0b9c11a7
André Roaldseth <andre.roaldseth@nav.no>
2020-03-04 11:08:25 +0100

    Add support for fetching inntekt without vedtakId
    Path of least resistance approach to support fetching inntekt without
    requiring a vedtakId.
    
    This will cause the inntekt to never be cached, and fetched from
    Inntektskomponenten every time.

--- src/main/kotlin/no/nav/dagpenger/inntekt/BehandlingsInntektsGetter.kt
+++ src/main/kotlin/no/nav/dagpenger/inntekt/BehandlingsInntektsGetter.kt
@@ -1,15 +1,21 @@
 package no.nav.dagpenger.inntekt
 
+import de.huxhorn.sulky.ulid.ULID
+import java.time.LocalDateTime
+import no.nav.dagpenger.inntekt.db.InntektId
 import no.nav.dagpenger.inntekt.db.InntektStore
 import no.nav.dagpenger.inntekt.db.StoredInntekt
 import no.nav.dagpenger.inntekt.inntektskomponenten.v1.InntektkomponentRequest
 import no.nav.dagpenger.inntekt.inntektskomponenten.v1.InntektskomponentClient
 import no.nav.dagpenger.inntekt.opptjeningsperiode.Opptjeningsperiode
 
-class BehandlingsInntektsGetter(private val inntektskomponentClient: InntektskomponentClient, private val inntektStore: InntektStore) {
+class BehandlingsInntektsGetter(
+    private val inntektskomponentClient: InntektskomponentClient,
+    private val inntektStore: InntektStore
+) {
+    private val ulidGenerator = ULID()
 
     suspend fun getBehandlingsInntekt(behandlingsKey: BehandlingsKey): StoredInntekt {
-
         val opptjeningsperiode = Opptjeningsperiode(behandlingsKey.beregningsDato)
 
         val inntektkomponentRequest = InntektkomponentRequest(
@@ -18,12 +24,36 @@ class BehandlingsInntektsGetter(private val inntektskomponentClient: Inntektskom
             opptjeningsperiode.sisteAvsluttendeKalenderMåned
         )
 
-        val storedInntekt = inntektStore.getInntektId(behandlingsKey)?.let { inntektStore.getInntekt(it) }
-            ?: inntektStore.insertInntekt(
-                behandlingsKey,
-                inntektskomponentClient.getInntekt(inntektkomponentRequest)
-            )
+        if (hasVedtakAssociation(behandlingsKey)) {
+            return getUnstoredInntekt(inntektkomponentRequest)
+        }
 
-        return storedInntekt
+        return isInntektStored(behandlingsKey)?.let { inntektStore.getInntekt(it) }
+            ?: fetchAndStoreInntekt(behandlingsKey, inntektkomponentRequest)
     }
+
+    private suspend fun fetchAndStoreInntekt(
+        behandlingsKey: BehandlingsKey,
+        inntektkomponentRequest: InntektkomponentRequest
+    ): StoredInntekt {
+        return inntektStore.insertInntekt(
+            behandlingsKey,
+            inntektskomponentClient.getInntekt(inntektkomponentRequest)
+        )
+    }
+
+    private fun isInntektStored(behandlingsKey: BehandlingsKey) =
+        inntektStore.getInntektId(behandlingsKey)
+
+    private suspend fun getUnstoredInntekt(inntektkomponentRequest: InntektkomponentRequest): StoredInntekt {
+        return StoredInntekt(
+            inntektId = InntektId(ulidGenerator.nextULID()),
+            inntekt = inntektskomponentClient.getInntekt(inntektkomponentRequest),
+            manueltRedigert = false,
+            timestamp = LocalDateTime.now()
+        )
+    }
+
+    private fun hasVedtakAssociation(behandlingsKey: BehandlingsKey) =
+        behandlingsKey.vedtakId == null
 }
