bce3c55be008590bef1126fa6a310ee9cd22d80a
Geir André Lund <geir.andre.lund@nav.no>
2020-03-24 12:31:58 +0100

    Fix INGEN_INNTEKT testcenario json

--- src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
+++ src/main/resources/db/testdata/R__testscenario_ingen_inntekt.sql
@@ -1,7 +1,13 @@
 INSERT INTO inntekt_v1(id, brukt ,inntekt)
-VALUES ('01E4644ZNDEEHKB2GWK8W27XBD', TRUE, '{}')
+VALUES ('01E46501PMY105AFXE4XF088MV', TRUE, '{
+  "arbeidsInntektMaaned": [],
+  "ident": {
+    "identifikator": "-1",
+    "aktoerType": "NATURLIG_IDENT"
+  }
+}')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO inntekt_v1_arena_mapping (inntektid, aktørid, vedtakid, beregningsdato)
-VALUES ('01E4644ZNDEEHKB2GWK8W27XBD', 'INGEN_INNTEKT', 12345, '2019-07-01')
+VALUES ('01E46501PMY105AFXE4XF088MV', 'INGEN_INNTEKT', 12345, '2019-07-01')
 ON CONFLICT DO NOTHING;
