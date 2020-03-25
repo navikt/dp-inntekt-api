88dcc81ce66fdc08a2e06547e4150600251d1289
André Roaldseth <andre.roaldseth@nav.no>
2020-03-25 19:08:07 +0100

    Add testdata for a Korona test scenario

---
+++ src/main/resources/db/testdata/R__testscenario_grunnbeløp_1_5_g_inntekt_korona.sql
@@ -0,0 +1,147 @@
+INSERT INTO inntekt_v1(id, brukt, inntekt)
+VALUES ('01D8G4TGH7Q9NKT7BPAG5XGXRT', TRUE, '
+{
+  "arbeidsInntektMaaned": [
+    {
+      "aarMaaned": "2019-11",
+      "arbeidsInntektInformasjon": {
+        "inntektListe": [
+          {
+            "inntektType": "LOENNSINNTEKT",
+            "beloep": 70000,
+            "fordel": "kontantytelse",
+            "inntektskilde": "A-ordningen",
+            "inntektsperiodetype": "Maaned",
+            "inntektsstatus": "LoependeInnrapportert",
+            "leveringstidspunkt": "2019-02",
+            "utbetaltIMaaned": "2018-03",
+            "opplysningspliktig": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "virksomhet": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "inntektsmottaker": {
+              "identifikator": "99999999999",
+              "aktoerType": "NATURLIG_IDENT"
+            },
+            "inngaarIGrunnlagForTrekk": true,
+            "utloeserArbeidsgiveravgift": true,
+            "informasjonsstatus": "InngaarAlltid",
+            "beskrivelse": "fastloenn"
+          }
+        ]
+      }
+    },
+    {
+      "aarMaaned": "2019-12",
+      "arbeidsInntektInformasjon": {
+        "inntektListe": [
+          {
+            "inntektType": "LOENNSINNTEKT",
+            "beloep": 25000,
+            "fordel": "kontantytelse",
+            "inntektskilde": "A-ordningen",
+            "inntektsperiodetype": "Maaned",
+            "inntektsstatus": "LoependeInnrapportert",
+            "leveringstidspunkt": "2019-02",
+            "utbetaltIMaaned": "2018-03",
+            "opplysningspliktig": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "virksomhet": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "inntektsmottaker": {
+              "identifikator": "99999999999",
+              "aktoerType": "NATURLIG_IDENT"
+            },
+            "inngaarIGrunnlagForTrekk": true,
+            "utloeserArbeidsgiveravgift": true,
+            "informasjonsstatus": "InngaarAlltid",
+            "beskrivelse": "fastloenn"
+          }
+        ]
+      }
+    },
+    {
+      "aarMaaned": "2020-01",
+      "arbeidsInntektInformasjon": {
+        "inntektListe": [
+          {
+            "inntektType": "LOENNSINNTEKT",
+            "beloep": 55000,
+            "fordel": "kontantytelse",
+            "inntektskilde": "A-ordningen",
+            "inntektsperiodetype": "Maaned",
+            "inntektsstatus": "LoependeInnrapportert",
+            "leveringstidspunkt": "2019-02",
+            "utbetaltIMaaned": "2018-03",
+            "opplysningspliktig": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "virksomhet": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "inntektsmottaker": {
+              "identifikator": "99999999999",
+              "aktoerType": "NATURLIG_IDENT"
+            },
+            "inngaarIGrunnlagForTrekk": true,
+            "utloeserArbeidsgiveravgift": true,
+            "informasjonsstatus": "InngaarAlltid",
+            "beskrivelse": "fastloenn"
+          }
+        ]
+      }
+    },
+    {
+      "aarMaaned": "2020-02",
+      "arbeidsInntektInformasjon": {
+        "inntektListe": [
+          {
+            "inntektType": "LOENNSINNTEKT",
+            "beloep": 25000,
+            "fordel": "kontantytelse",
+            "inntektskilde": "A-ordningen",
+            "inntektsperiodetype": "Maaned",
+            "inntektsstatus": "LoependeInnrapportert",
+            "leveringstidspunkt": "2019-02",
+            "utbetaltIMaaned": "2018-03",
+            "opplysningspliktig": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "virksomhet": {
+              "identifikator": "1111111",
+              "aktoerType": "ORGANISASJON"
+            },
+            "inntektsmottaker": {
+              "identifikator": "99999999999",
+              "aktoerType": "NATURLIG_IDENT"
+            },
+            "inngaarIGrunnlagForTrekk": true,
+            "utloeserArbeidsgiveravgift": true,
+            "informasjonsstatus": "InngaarAlltid",
+            "beskrivelse": "fastloenn"
+          }
+        ]
+      }
+    }
+  ],
+  "ident": {
+    "identifikator": "-1",
+    "aktoerType": "NATURLIG_IDENT"
+  }
+}')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO inntekt_v1_arena_mapping (inntektid, aktørid, vedtakid, beregningsdato)
+VALUES ('01D8G4TGH7Q9NKT7BPAG5XGXRT', '1.5_G_INNTEKT_KORONA', 12345, '2020-03-21')
+ON CONFLICT DO NOTHING;;
