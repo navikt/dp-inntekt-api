19ac528942545dd9eb1b0722fa59d9a4ab0c8e65
Giao The Cung <giaothe@gmail.com>
2020-06-04 14:03:30 +0200

    Hent inntekter fra q1.

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Configuration.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Configuration.kt
@@ -41,7 +41,7 @@ private val devProperties = ConfigurationMap(
         "database.name" to "dp-inntekt-db-preprod",
         "flyway.locations" to "db/migration,db/testdata",
         "vault.mountpath" to "postgresql/preprod-fss/",
-        "hentinntektliste.url" to "https://app-q0.adeo.no/inntektskomponenten-ws/rs/api/v1/hentinntektliste",
+        "hentinntektliste.url" to "https://app-q1.adeo.no/inntektskomponenten-ws/rs/api/v1/hentinntektliste",
         "enhetsregisteret.url" to "https://data.brreg.no/enhetsregisteret/api",
         "oppslag.url" to "http://dagpenger-oppslag/api",
         "oidc.sts.issuerurl" to "https://security-token-service.nais.preprod.local/",
