34cc5dffe01c919bf5876bd4a75c7c3ca1a3d26e
Giao The Cung <giaothe@gmail.com>
2020-07-16 12:30:33 +0200

    Fix table name.

---
+++ dp-inntekt-api/src/main/resources/db/migration/V8__Fix_table_name.sql
@@ -0,0 +1,4 @@
+ALTER TABLE temp_inntekt_V1_person_mapping_
+    RENAME TO temp_inntekt_V1_person_mapping;
+
+
--- dp-inntekt-api/src/test/kotlin/no/nav/dagpenger/inntekt/db/PostgresTest.kt
+++ dp-inntekt-api/src/test/kotlin/no/nav/dagpenger/inntekt/db/PostgresTest.kt
@@ -33,7 +33,7 @@ internal class PostgresTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = migrate(DataSource.instance)
-            assertEquals(11, migrations, "Wrong number of migrations")
+            assertEquals(12, migrations, "Wrong number of migrations")
         }
     }
 
@@ -51,7 +51,7 @@ internal class PostgresTest {
     fun `Migration of testdata `() {
         withCleanDb {
             val migrations = migrate(DataSource.instance, locations = listOf("db/migration", "db/testdata"))
-            assertEquals(16, migrations, "Wrong number of migrations")
+            assertEquals(17, migrations, "Wrong number of migrations")
         }
     }
 
