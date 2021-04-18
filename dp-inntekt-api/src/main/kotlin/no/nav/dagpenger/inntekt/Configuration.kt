e57ccfb791e9caf94427f52b1f036a3f7f5fa666
Giao The Cung <giaothe@gmail.com>
2021-04-18 18:52:58 +0200

    Step 1: Bytte til dp-inntekt-db-q0 for å få med flyway migreringer

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Configuration.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Configuration.kt
@@ -41,9 +41,9 @@ private val localProperties = ConfigurationMap(
 )
 private val devProperties = ConfigurationMap(
     mapOf(
-        "database.host" to "b27dbvl007.preprod.local",
+        "database.host" to "b27dbvl013.preprod.local",
         "database.port" to "5432",
-        "database.name" to "dp-inntekt-db-preprod",
+        "database.name" to "dp-inntekt-db-q0",
         "flyway.locations" to "db/migration,db/testdata",
         "vault.mountpath" to "postgresql/preprod-fss/",
         "hentinntektliste.url" to "https://app-q1.adeo.no/inntektskomponenten-ws/rs/api/v1/hentinntektliste",
